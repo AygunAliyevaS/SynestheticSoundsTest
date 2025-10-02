6502e074d42b078fb3f3a65758c83fe41f5521f8
Lantop <41171274+Lantop1k@users.noreply.github.com>
2025-10-02 20:20:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "✅ Flask test app is running on Azure!"
+    return render_template("index.html")
+
+@app.route("/gif/<filename>")
+def serve_gif(filename):
+    return send_from_directory("static", filename)
+
+@app.route("/health")
+def health():
+    return "OK", 200
+
+@app.route("/robots933456.txt")
+def robots():
+    return "User-agent: *\nDisallow:", 200, {"Content-Type": "text/plain"}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
