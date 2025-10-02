3de137903ed183fdb22743ff0c7d2a2a5a2f0916
Lantop <41171274+Lantop1k@users.noreply.github.com>
2025-10-02 00:35:17 -1100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
-def main():
-    return "Everything is good"
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "✅ Flask test app is running on Azure!"
 
 if __name__ == "__main__":
-    print(main())
+    app.run(host="0.0.0.0", port=8000)
